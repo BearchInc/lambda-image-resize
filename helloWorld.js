ee8d5663d9901d10a8789753c403dbe876de1a1a
Juraci/Lisardo <lisardo.kist@gmail.com>
2016-04-25 17:35:11 -0300

    [#38] Basic resize of png images.

--- helloWorld.js
+++ helloWorld.js
@@ -1,9 +1,12 @@
+var gm = require('gm').subClass({
+    imageMagick: true
+});
 var AWS = require('aws-sdk');
 var async = require('async');
 
 var s3 = new AWS.S3();
 
-exports.handler = function (event, context) {
+exports.handler = function(event, context) {
     console.log('event: ' + JSON.stringify(event));
 
     var obj = {
@@ -15,33 +18,34 @@ exports.handler = function (event, context) {
     async.waterfall([
         function download(next) {
             // get Markdown object
-            s3.getObject(
-                {
-                    Bucket: obj.bucket,
-                    Key: obj.key
-                },
-                next);
+            s3.getObject({
+                Bucket: obj.bucket,
+                Key: obj.key
+            },
+            next);
         },
         function transform(response, next) {
-            // convert md -> html
-            //var data = marked(String(response.Body));
-
-            next(null, response.Body);
+            gm(response.Body).size(function(err, size) {
+                var width = size.width * 0.8;
+                var height = size.height * 0.8;
+                this.resize(width, height).toBuffer('PNG', function(err, buffer) {
+                    next(null, buffer);
+                });
+            });
         },
         function upload(data, next) {
             // change file extension
             var newFileName = obj.key.split(".")[0] + "-renamed.png";
             console.log("Uploading data to: " + obj.bucketOut);
-            s3.putObject(
-                {
-                    Bucket: obj.bucketOut,
-                    Key: newFileName,
-                    Body: data,
-                    ContentType: "image/png" // set contentType as HTML
-                },
-                next);
+            s3.putObject({
+                Bucket: obj.bucketOut,
+                Key: newFileName,
+                Body: data,
+                ContentType: "image/png" // set contentType as HTML
+            },
+            next);
         }
-    ], function (err) {
+    ], function(err) {
         if (err) {
             console.error(err);
         } else {
