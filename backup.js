1590dac0fce6a075c8c5d83e08e6373f8b680115
Lisardo <lisardo.kist@gmail.com>
2016-04-26 16:26:30 -0300

    [#38] Check if exists.

--- backup.js
+++ backup.js
@@ -5,7 +5,7 @@ var s3 = new AWS.S3();
 
 exports.handler = function(event, context) {
     console.log('event: ' + JSON.stringify(event));
-    
+
     var obj = {
         'bucket': event.Records[0].s3.bucket.name,
         'bucketOut': event.Records[0].s3.bucket.name + "-backup",
@@ -13,6 +13,19 @@ exports.handler = function(event, context) {
     };
 
     async.waterfall([
+        function checkExists(next) {
+            s3.headObject({
+                'Bucket': obj.bucketOut,
+                'Key': obj.key
+            }, function(err, data) {
+                if (err) {
+                    console.log('File does not exist, creating...');
+                    next(null, next);
+                } else {
+                    next('Aborting. File already exists.');
+                }
+            })
+        },
         function copy(next) {
             s3.copyObject({
                 Bucket: obj.bucketOut,
